c95f60a6dd303cab7b088da6d7ec5649032133dd
Luciano Pennacchioni <luciano.p3196@gmail.com>
2022-04-03 02:16:07 -0300

    Footer is implemented

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,25 +1,48 @@
 import React from "react";
 import Container from "@mui/material/Container";
-import { Box } from "@mui/system";
+import Box from "@mui/system/Box";
+import style from "./Footer.module.css";
+import FacebookRoundedIcon from "@mui/icons-material/FacebookRounded";
+import InstagramIcon from "@mui/icons-material/Instagram";
+import WhatsappOutlinedIcon from "@mui/icons-material/WhatsappOutlined";
+import LocationOnOutlinedIcon from "@mui/icons-material/LocationOnOutlined";
+import logo from "../../assets/logo.jpg";
 
 export default function Footer() {
   return (
-    <footer>
+    <footer className={style.footer}>
       <Container
         sx={{
           display: "flex",
           flexDirection: "row",
           justifyContent: "space-between",
+          alignItems: "center",
         }}
-        positions={{ bottom: "0px" }}
       >
-        <h3>Más Conciencia (LOGO)</h3>
-        <p> Contactanos - Tel: +542355-521097 </p>
-        <Box>
-          <p>Localidad de Lincoln</p>
-          <p>Buenos Aires</p>
+        <img src={logo} alt="LOGO" width={50}/>
+
+        <Box
+          component="p"
+          sx={{
+            display: "flex",
+            flexDirection: "row",
+            alignItems: "center",
+          }}
+        >
+          <LocationOnOutlinedIcon sx={{ mr: 1, color: "primary.light" }} />{" "}
+          Lincoln - Buenos aires
         </Box>
-        <p>Poner íconos de redes sociales</p>
+        <Box>
+          <FacebookRoundedIcon color="primary" />
+          <InstagramIcon sx={{ color: "secondary.light", mx: 3 }} />
+          <a
+            href="https://api.whatsapp.com/send?phone=542215698473"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <WhatsappOutlinedIcon sx={{ color: "success.light" }} />
+          </a>
+        </Box>
       </Container>
     </footer>
   );
