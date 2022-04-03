a58bb6c3118aff6153622e94d5c159ed5973b2e8
Luciano Pennacchioni <luciano.p3196@gmail.com>
2022-04-03 14:07:39 -0300

    Effects in link to project (Project.jsx)

--- src/components/Projects/ButtsProject/ButtsProject.jsx
+++ src/components/Projects/ButtsProject/ButtsProject.jsx
@@ -53,7 +53,7 @@ export default function ButtsProject() {
       </ul>
       <span>Puntos de recolección:</span>
       <WrappedMap
-        googleMapURL={`https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=${process.env.MAP_API_KEY}`}
+        googleMapURL={`https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=${process.env.REACT_MAP_API_KEY}`}
         loadingElement={<div style={{ height: `100%` }} />}
         containerElement={<div style={{ height: `400px` }} />}
         mapElement={<div style={{ height: `100%` }} />}
--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -5,14 +5,22 @@ import { imagesCarousel } from "./ButtsProject/ButtsProject";
 import { Box } from "@mui/system";
 import { Container } from "@mui/material";
 import cigarro1 from "../../assets/butts/cigarro1.jpg";
+import style from "./Projects.module.css";
+import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
 
 export default function Projects() {
   return (
     <Container>
-      <Box component={"h2"} sx={{ fontSize: "h5.fontSize", color: "info.main" }}>Conocé nuestros proyectos</Box>
+      <Box
+        component={"h2"}
+        sx={{ fontSize: "h5.fontSize", color: "info.main" }}
+      >
+        Conocé nuestros proyectos
+      </Box>
       <Box sx={{ display: "flex", justifyContent: "flex-start" }}>
         <PhotoCarousel imagesURL={imagesCarousel} />
-        <Box ml={10} sx={{ display: "flex", alignItems: "flex-start" }}>
+
+        <Link to="/colillas-de-cigarrillos" className={style.project__Link}>
           <img
             src={cigarro1}
             alt="Cigarrillo encendido"
@@ -20,15 +28,11 @@ export default function Projects() {
             height={80}
             style={{ marginRight: 30 }}
           />
-          <Link
-            to="/colillas-de-cigarrillos"
-            style={{ textDecoration: "none" }}
-          >
-            <Box component={"h2"}>
-              Colillas de cigarrillos
-            </Box>
-          </Link>
-        </Box>
+          <Box component={"h2"}>
+            Colillas de cigarrillos
+          </Box>
+          <ArrowForwardIosIcon sx={{ color: "#dedede", ml: 8 }} />
+        </Link>
       </Box>
     </Container>
   );
